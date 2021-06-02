b30f103fb5e9442630543794fe1dc984fb16e930
SunilDeveloper7 <sunnil474@gmail.com>
2021-06-02 15:57:51 -0700

    Added tasks and task components and created delete and toggle functionallity

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks'
+import { useState} from 'react' 
 // import React from 'react'
 
 function App() {
+  const [tasks,setTasks] = useState([
+    {
+    id:1,
+    text:'Doctors Appointment',
+    day: 'sunday @ 2:30pm',
+    reminder:true,
+
+},
+{
+    id:2,
+    text:'Doctors Appointment',
+    day: 'sunday @ 2:30pm',
+    reminder:true,
+
+},
+{
+    id:3,
+    text:'Doctors Appointment',
+    day: 'sunday @ 2:30pm',
+    reminder:true,
+
+},
+{
+    id:4,
+    text:'Doctors Appointment',
+    day: 'sunday @ 2:30pm',
+    reminder:true,
+
+}])
+
+
+const deleteTask =(id) => {
+  setTasks(tasks.filter((task)=> task.id !==id))
+}
+
+
+//Toggle remindar
+const toggleReminder =(id) => {
+  setTasks(
+    tasks.map((task)=> 
+    task.id===id ? {...task,reminder: 
+    !task.reminder } : task
+    )
+  )
+}
+
+
   return (
     <div className="container">
           <Header  />
+          {tasks.length > 0 ? (
+            <Tasks tasks={tasks} 
+          onDelete={deleteTask}  onToggle={toggleReminder}/>
+  ) : ('No task to show'
+  )}
     </div>
-  );
+  )
 }
 // class App extends React.Component {
 //   render() {
