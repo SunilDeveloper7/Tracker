5743a6f6b2c7ab62dba7ab6bdb9598b516d0d373
SunilDeveloper7 <sunnil474@gmail.com>
2021-06-08 10:49:15 -0700

    completed all componenets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types';
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick=()=> {
-        console.log('Click')
-    }
+const Header = ({title,onAdd,showAdd}) => {
     return (
         <header className='header'>
         <h1>{title}</h1>
-        < Button color='coral' text='Add' onClick={onClick} />
+        < Button color={showAdd ? 'red': 'maroon'} 
+        text={showAdd ? 'close': 'Add'} 
+        onClick={onAdd} />
         </header>
     )
 }
